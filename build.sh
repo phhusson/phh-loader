b04908fbe0daebf649c9f4a55ec1bf1a2ec31bc3
Pierre-Hugues Husson <phh@phh.me>
2020-06-24 21:01:22 +0200

    Fix dhcp not setting routes/ips and include kexec

--- build.sh
+++ build.sh
@@ -8,4 +8,11 @@ mkdir root/bin
 cp busybox-armv8l root/bin
 chmod 0755 root/bin/busybox-armv8l
 ln -s /bin/busybox-armv8l root/bin/sh
+cp kexec root/bin
+chmod 0755 root/bin/kexec
+
+mkdir -p root/usr/share/udhcpc/
+cp simple.script root/usr/share/udhcpc/default.script
+chmod 0755 root/usr/share/udhcpc/default.script
+
 (cd root; find | cpio -o -H newc) |gzip -9 -c > initramfs
